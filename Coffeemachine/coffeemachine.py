fee5dd6da27384df9c06cd622a80f4f5aae056db
fuckpythonallmyhomiesusejs <b.a.zharikov@student.khai.edu>
2023-01-05 15:18:00 +0200

    coffeemachine.py

--- Coffeemachine/coffeemachine.py
+++ Coffeemachine/coffeemachine.py
@@ -1,141 +1,108 @@
-water = 400
-milk = 540
-beans = 120
-cups = 9
-money = 550
+class CoffeeMachine:
+    running = False
+
+    def __init__(self, water, milk, coffee_beans, cups, money):
+        """resources in coffee machine"""
+        self.water = water
+        self.milk = milk
+        self.beans = coffee_beans
+        self.cups = cups
+        self.money = money
+
+        if not CoffeeMachine.running:
+            """ if the machine isn't running then start running"""
+            self.buy_menu()
+
+    def buy_menu(self):
+        """not to run start() in a method use self.running & action selection"""
+        self.running = True
+        self.action = input("\n(buy, fill, take, remaining, exit)\nВыберите: ")
+        print()
+        action_choices = {"buy": self.buy, "fill": self.fill_menu, "take": self.take, "exit": exit, "remaining": self.remaining}
+        if self.action in action_choices:
+            action_choices[self.action]()
+        else:
+            exit()
+
+    def return_to_menu(self):
+        """returns to the menu after an action"""
+        print()
+        self.buy_menu()
+
+    def available_check(self):
+        """checks if he can afford to prepare such coffee at the moment & checks if the resources goes down to 0"""
+        self.not_available = ""
+        if self.water - self.reduced[0] < 0:
+            self.not_available = "water"
+        elif self.milk - self.reduced[1] < 0:
+            self.not_available = "milk"
+        elif self.beans - self.reduced[2] < 0:
+            self.not_available = "beans"
+        elif self.cups - self.reduced[3] < 0:
+            self.not_available = "cups"
+
+        if self.not_available != "":
+            print(f"Извините, не хватает {self.not_available}!")
+            return False
+        else:
+            print("У меня достаточно ресурсов, чтобы сделать кофе!")
+            return True
+
+    def get_ing_count(self):
+        """performs an operation"""
+        self.water -= self.reduced[0]
+        self.milk -= self.reduced[1]
+        self.beans -= self.reduced[2]
+        self.cups -= self.reduced[3]
+        self.money += self.reduced[4]
+
+    def buy(self):
+        """menu & checking for consumables """
+        self.choice = input("Что вы хотите купить? \n1 - espresso \n2 - latte \n3 - cappuccino \nback - выйти в меню\n")
+        if self.choice == '1':
+            self.reduced = [250, 0, 16, 1, 4]
+            if self.available_check():
+                self.get_ing_count()
+
+        elif self.choice == '2':
+            self.reduced = [350, 75, 20, 1, 7]
+            if self.available_check():
+                self.get_ing_count()
+
+        elif self.choice == "3":
+            self.reduced = [200, 100, 12, 1, 6]
+            if self.available_check():
+                self.get_ing_count()
+
+        elif self.choice == "back":
+            self.return_to_menu()
+
+        self.return_to_menu()
+
+    def fill_menu(self):
+        """adding consumables"""
+        self.water += int(input("Напишите, сколько мл. воды вы хотите добавить:\n"))
+        self.milk += int(input("Напишите, сколько мл. молока вы хотите добавить::\n"))
+        self.beans += int(input("Напишите, сколько граммов зерен вы хотите добавить\n"))
+        self.cups += int(input("Напишите, сколько одноразовых кофейных стаканчиков вы хотите добавить:\n"))
+        self.return_to_menu()
+
+    def take(self):
+        """cash withdrawal"""
+        print(f"I gave you ${self.money}")
+        self.money -= self.money
+        self.return_to_menu()
+
+    def remaining(self):
+        """displaying the amount of consumables"""
+        print(f"В кофемашине есть:")
+        print(f"{self.water} воды")
+        print(f"{self.milk} молока")
+        print(f"{self.beans} зерен")
+        print(f"{self.cups} одноразовых стаканчиков")
+        print(f"${self.money} денег")
+        self.return_to_menu()
 
 
-def start():
-    menu = "Выберите: \n(buy, fill, take, remaining, exit)\n"
-    choice = None
-
-    while choice != "exit":
-        choice = input(menu).strip()
-
-        if choice == "buy":
-            buy_menu()
-
-        elif choice == "fill":
-            fill_menu()
-
-        elif choice == "take":
-            take()
-
-        elif choice == "remaining":
-            rem()
-
-
-def buy_menu():
-    back = False
-
-    while not back:
-        select = "Что вы хотите купить? \n1 - espresso \n2 - latte \n3 - cappuccino \nback - выйти в меню\n"
-        choice = input(select).strip()
-
-        if choice == "back":
-            back = True
-
-        elif choice.isdigit():
-            int_choice =int(choice)
-
-            if 0 < int_choice <= 3:
-                get_coffee(int_choice)
-                back = True
-
-
-def get_coffee(choice: int):
-    global water, milk, beans, money, cups
-    ing_list = get_ing_count(choice)
-    drink_names = ["espresso", "latte", "cappuccino"]
-
-    if check_ing(ing_list):
-        water -= ing_list[0]
-        milk -= ing_list[1]
-        beans  -= ing_list[2]
-        money += ing_list[3]
-        cups -= 1
-        print("У меня достаточно ресурсов, чтобы сделать {0}!".format(drink_names[choice-1]))
-
-
-
-def check_ing(ing_list: list):
-    global water, milk, beans, cups
-
-    if water < ing_list[0]:
-        print("Извините, мало воды")
-        return False
-
-    elif milk < ing_list[1]:
-        print("Извините, мало молока")
-        return False
-
-    elif beans  < ing_list[2]:
-        print("Извините, не хватает кофейных зерен")
-        return False
-
-    elif cups < 1:
-        print("Извините, не хватает чашек")
-        return False
-
-    return True
-
-
-def get_ing_count(choice: int):
-    list_ing = [0, 0, 0, 0]
-
-    if choice == 1:
-        list_ing[0] = 250
-        list_ing[2] = 16
-        list_ing[3] = 4
-
-    elif choice == 2:
-        list_ing[0] = 350
-        list_ing[1] = 75
-        list_ing[2] = 20
-        list_ing[3] = 7
-
-    else:
-        list_ing[0] = 200
-        list_ing[1] = 100
-        list_ing[2] = 12
-        list_ing[3] = 6
-
-    return list_ing
-
-
-def fill_menu():
-    global water, milk, beans, cups
-
-    ing = int(input("Напишите, сколько мл. воды вы хотите добавить: "))
-    water += ing
-
-    ing = int(input("Напишите, сколько мл. молока вы хотите добавить: "))
-    milk += ing
-
-    ing = int(input("Напишите, сколько граммов зерен вы хотите добавить: "))
-    beans  += ing
-
-    ing = int(input("Напишите, сколько одноразовых кофейных стаканчиков вы хотите добавить: "))
-    cups += ing
-
-
-
-def take():
-    global money
-
-    print(f"Сдача {money}")
-    money = 0
-    remaining()
-    ...
-
-
-def remaining():
-    global water, milk, beans , cups, money
-
-    print("В кофемашине есть: \n{0} воды \n{1} Молока"
-          "\n{2} зерен \n{3} одноразовых стаканчиков \n{4} денег\n"
-          .format(water, milk, beans, cups, money))
-
-
-
-start()
+CoffeeMachine(400, 540, 120, 9, 550)
+"""launch based on resources in parentheses"""
